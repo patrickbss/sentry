45754c4c14388bdd5c81804bb0ebdad1bf23c73e
Burak Yigit Kaya <ben@byk.im>
2020-05-25 01:44:57 +0300

    build(docker): Split builder image out for better caching
    This is an experimental new approach which creates a separate builder image, that is highly cacheable (and allows caching for yarn install step) to generate the Python wheel. It should save us multiple minutes by leveraging our builds' incremental nature much better.

---
+++ docker/builder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f setup.py ]; then
+    >&2 echo "Cannot find setup.py, make sure you have mounted your source dir to /workspace"
+    exit 1
+fi
+
+if [ ! -d "node_modules" ]; then
+  mkdir node_modules
+fi
+echo "Populating node_modules cache..."
+cp -ur /js/node_modules/* ./node_modules/
+chmod -R +x ./bin
+
+export SENTRY_BUILD=${SOURCE_COMMIT:-unknown}
+export YARN_CACHE_FOLDER="$(mktemp -d)"
+python setup.py bdist_wheel
+rm -r "$YARN_CACHE_FOLDER"
+pkginfo -f requires_dist --single --sequence-delim=! dist/*.whl | tr ! \\n > dist/requirements.txt
